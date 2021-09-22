09f7c130308fdf2026082762a8eec4e1321b074a
ssinghbb <santosh.singh@imentus.com>
2021-09-22 18:43:45 +0530

    create front end with  react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,136 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import Lottery from './Lottery';
+import { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+
+
 
 function App() {
+  const [manager, setManager] = useState('')
+  const [players, setPLayers] = useState('')
+  const [balance, setBalance] = useState('')
+  const [etherValue, setEtherValue] = useState('')
+  const [status, setStatus] = useState('')
+  const [winStatus, setWinStatus] = useState('')
+
+  useEffect(async () => {
+    console.log("mama", Lottery)
+    let manager = await Lottery.methods.manager().call();
+    let players = await Lottery.methods.getPlayers().call();
+    let balance = await web3.eth.getBalance(Lottery.options.address);
+    let accounts = web3.eth.getAccounts();
+    console.log("aaaacaca", accounts)
+    setManager(manager)
+    setPLayers(players)
+    setBalance(balance)
+  }, [])
+
+  //submit form
+  const submitEther = async (e) => {
+    e.preventDefault();
+    let accounts = await web3.eth.getAccounts();
+    console.log("acc", accounts)
+    setStatus(<div className="spinner-border text-primary" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>)
+    await Lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(etherValue, 'ether')
+    })
+    setStatus('You have been entered ...!')
+
+  }
+
+
+  //pick a winner function 
+  const pickWinner = async () => {
+    let accounts = await web3.eth.getAccounts();
+    console.log("accounts", accounts)
+
+    setWinStatus(<div className="spinner-border text-primary" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>)
+
+    await Lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    setWinStatus('A winner has been picked ...!')
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="font-weight-bolder">Take the chance to change your life</h1>
+            <p>Sorteo is online lottery platform inspired by few sorteo lover's fantasy of the ultimate lottery platfrom.</p>
+            <button className="btn btn-block btn-primary rounded-pill">BUY TICKET NOW!</button>
+            </div>
+        <div className="col-md-6">
+          <img src='/logo192.png'/>
+        </div>
+        </div>
+      </div>
+    <div className="container-fluid lottery">
+      <div className="row">
+        <div className="col-md-6">
+          <h4>Buy Lottery Tickets Online</h4>
+          <p>Buy lottery tickets online to the biggest lotteries in the world offering huge jackpot prizes that you can win when you play online lottery.</p>
+        </div>
+        <div className="col-md-3"> </div>
+        <div className="col-md-3 ">
+          <button className="btn mb-3 btn-block bg-success text-white rounded-pill">Register & Play</button>
+          <p className="px-3">View all offer</p>
+        </div>
+      </div>
+    </div> */}
+
+
+
+
+
+
+
+
+
+
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2 className="">Lottery Contract</h2>
+            <p>This contract is managed by <span className="" style={{ color: 'red' }}> {manager}</span> <br />
+              There are currently  {players.length} people entered, competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+            </p>
+         
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <form onSubmit={submitEther}>
+              <h3>Want to try your luck</h3>
+              <label>Amount of ether to enter</label>
+              <input type="text" name="" value={etherValue} onChange={(e) => setEtherValue(e.target.value)} id="" />
+              <button>Enter </button> {status}
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* pick a winner  */}
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <p>Ready to pick a Winner ?</p>
+            <button onClick={pickWinner}>Pick a winner!</button> {winStatus}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <div className="container">
+            <nav className="navbar navbar-expand-xl  ">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"><img src='https://pixner.net/sorteo/Sorteo/assets/images/logo.png'/></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 " >
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li className="nav-item">
+        <NavLink exact to="/profile">
+  Results
+</NavLink>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            LOTTERIES
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            BLOG
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PAGES
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">CONTACT US</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+        </div>
+    )
+}
